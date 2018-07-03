fbe74baa280dc87c71fc5266e9391ea258d4cf1d
oppahansi <oppahansi@gmail.com>
2018-07-03 22:14:20 +0200

    Adding initial project files. WIP

---
+++ src/com/oppahansi/Racg.java
@@ -0,0 +1,22 @@
+package com.oppahansi;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Racg extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("fxml/RACG.fxml"));
+        Scene scene = new Scene(root);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("RACG");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
---
+++ src/com/oppahansi/controller/RacgController.java
@@ -0,0 +1,164 @@
+package com.oppahansi.controller;
+
+import javafx.fxml.FXML;
+import javafx.scene.Group;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ToggleGroup;
+import javafx.scene.layout.Pane;
+
+public class Main {
+
+    @FXML
+    private Group typeGroup;
+
+    @FXML
+    private ToggleGroup type;
+
+    @FXML
+    private Pane subTitle;
+
+    @FXML
+    private Group typeGroup1;
+
+    @FXML
+    private ToggleGroup searchCheckTitle;
+
+    @FXML
+    private Pane subDomain;
+
+    @FXML
+    private Group typeGroup11;
+
+    @FXML
+    private ToggleGroup searchCheckTitle1;
+
+    @FXML
+    private Pane subUrl;
+
+    @FXML
+    private Group typeGroup12;
+
+    @FXML
+    private ToggleGroup searchCheckTitle2;
+
+    @FXML
+    private Pane subBody;
+
+    @FXML
+    private CheckBox subBodyChecked;
+
+    @FXML
+    private Group typeGroup13;
+
+    @FXML
+    private ToggleGroup searchCheckTitle3;
+
+    @FXML
+    private Pane subFlairText;
+
+    @FXML
+    private CheckBox subFlairTextChecked;
+
+    @FXML
+    private Group typeGroup111;
+
+    @FXML
+    private ToggleGroup searchCheckTitle11;
+
+    @FXML
+    private Pane subFlairCss;
+
+    @FXML
+    private CheckBox subFlairCssChecked;
+
+    @FXML
+    private Group typeGroup121;
+
+    @FXML
+    private ToggleGroup searchCheckTitle21;
+
+    @FXML
+    private Pane userName;
+
+    @FXML
+    private CheckBox userNameChecked;
+
+    @FXML
+    private Group typeGroup1211;
+
+    @FXML
+    private ToggleGroup searchCheckTitle211;
+
+    @FXML
+    private Pane userName1;
+
+    @FXML
+    private CheckBox userNameChecked1;
+
+    @FXML
+    private Group typeGroup12111;
+
+    @FXML
+    private ToggleGroup searchCheckTitle2111;
+
+    @FXML
+    private Pane userName11;
+
+    @FXML
+    private CheckBox userNameChecked11;
+
+    @FXML
+    private Group typeGroup121111;
+
+    @FXML
+    private ToggleGroup searchCheckTitle21111;
+
+    @FXML
+    private Pane userName111;
+
+    @FXML
+    private CheckBox userNameChecked111;
+
+    @FXML
+    private Group typeGroup1211111;
+
+    @FXML
+    private ToggleGroup searchCheckTitle211111;
+
+    @FXML
+    private Pane userName1111;
+
+    @FXML
+    private CheckBox userNameChecked1111;
+
+    @FXML
+    private Group typeGroup12111111;
+
+    @FXML
+    private ToggleGroup searchCheckTitle2111111;
+
+    @FXML
+    private Group typeGroup2;
+
+    @FXML
+    private ToggleGroup actionType;
+
+    @FXML
+    private Pane userName2;
+
+    @FXML
+    private CheckBox userNameChecked2;
+
+    @FXML
+    private Pane userName21;
+
+    @FXML
+    private CheckBox userNameChecked21;
+
+    @FXML
+    private Pane userName211;
+
+    @FXML
+    private CheckBox userNameChecked211;
+
+}
